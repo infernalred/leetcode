b030314fb8118c356f5f961f8571ed3659772bf2
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-10-28 18:22:10 +0300

    Create MiddleOfTheLinkedList.cs

---
+++ Leetcode/Leetcode/MiddleOfTheLinkedList.cs
@@ -0,0 +1,30 @@
+﻿namespace Leetcode;
+
+public class MiddleOfTheLinkedList
+{
+    public ListNode MiddleNode(ListNode head)
+    {
+        var slow = head;
+        var fast = head;
+
+        while (fast?.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
