c9c944e1a6f9f47015d55a251745e5bde59e75b8
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-10-28 11:41:26 +0300

    ransonnote

---
+++ Leetcode/LeetCode.Tests/RansomNoteTests.cs
@@ -0,0 +1,59 @@
+﻿using Leetcode;
+using Xunit;
+
+namespace LeetCode.Tests;
+
+public class RansomNoteTests
+{
+    [Fact]
+    public void Test1()
+    {
+        const string ransomNote = "a"; 
+        const string magazine = "b";
+
+        const bool expect = false;
+
+        var actual = RansomNote.CanConstruct(ransomNote, magazine);
+        
+        Assert.Equal(expect, actual);
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        const string ransomNote = "aa"; 
+        const string magazine = "ab";
+
+        const bool expect = false;
+
+        var actual = RansomNote.CanConstruct(ransomNote, magazine);
+        
+        Assert.Equal(expect, actual);
+    }
+    
+    [Fact]
+    public void Test3()
+    {
+        const string ransomNote = "aa"; 
+        const string magazine = "aab";
+
+        const bool expect = true;
+
+        var actual = RansomNote.CanConstruct(ransomNote, magazine);
+        
+        Assert.Equal(expect, actual);
+    }
+    
+    [Fact]
+    public void Test4()
+    {
+        const string ransomNote = "aab"; 
+        const string magazine = "baa";
+
+        const bool expect = true;
+
+        var actual = RansomNote.CanConstruct(ransomNote, magazine);
+        
+        Assert.Equal(expect, actual);
+    }
+}
---
+++ Leetcode/Leetcode/RansomNote.cs
@@ -0,0 +1,41 @@
+﻿namespace Leetcode;
+
+public static class RansomNote
+{
+    public static bool CanConstruct(string ransomNote, string magazine) 
+    {
+        //slowly but memory usage less then 60%
+        
+
+        // var queue = new Queue<char>(ransomNote);
+        // for (int i = 0; i < ransomNote.Length; i++)
+        // {
+        //     var letter = queue.Dequeue();
+        //     var index = magazine.IndexOf(letter);
+        //     if (index >= 0)
+        //     {
+        //         
+        //         magazine = magazine.Remove(index, 1);
+        //     }
+        //     else
+        //     {
+        //         queue.Enqueue(letter);
+        //     }
+        // }
+        // return queue.Count == 0;
+        
+        //faster but more memory for array
+        var chars = new int[256];
+        foreach (var c in magazine)
+        {
+            chars[c]++;
+        }
+
+        foreach (var c in ransomNote)
+        {
+            chars[c]--;
+            if (chars[c] < 0) return false;
+        }
+        return true;
+    }
+}
