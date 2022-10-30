a3105eff5ddc176e58654892d7e6bab91fad0cf1
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-10-30 21:38:12 +0300

    add TheKWeakestRowsInAMatrix

---
+++ Leetcode/LeetCode.Tests/TheKWeakestRowsInAMatrixTest.cs
@@ -0,0 +1,27 @@
+﻿using Leetcode;
+using Xunit;
+
+namespace LeetCode.Tests;
+
+public class TheKWeakestRowsInAMatrixTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var mat = new []
+        {
+            new []{1,1,0,0,0},
+            new []{1,1,1,1,0},
+            new []{1,0,0,0,0},
+            new []{1,1,0,0,0},
+            new []{1,1,1,1,1}
+        }; 
+        const int k = 3;
+
+        var expect = new []{2,0,3};
+
+        var actual = TheKWeakestRowsInAMatrix.KWeakestRows(mat, k);
+        
+        Assert.Equal(expect, actual);
+    }
+}
---
+++ Leetcode/Leetcode/TheKWeakestRowsInAMatrix.cs
@@ -0,0 +1,26 @@
+﻿namespace Leetcode;
+
+public static class TheKWeakestRowsInAMatrix
+{
+    public static int[] KWeakestRows(int[][] mat, int k)
+    {
+        //old
+        // var dic = new Dictionary<int, int>();
+        // for (int i = 0; i < mat.Length; i++)
+        // {
+        //     var row = mat[i].Sum();
+        //     dic.Add(i, row);
+        // }
+        // var dicNew = dic.OrderBy(x => x.Value).Take(k).Select(x => x.Key).ToArray();
+        // return dicNew;
+        
+        //after refactor
+
+        return mat
+            .Select((x, y) => (y, cnt: x.Sum()))
+            .OrderBy(x => x.cnt)
+            .Select(x => x.y)
+            .Take(k)
+            .ToArray();
+    }
+}
