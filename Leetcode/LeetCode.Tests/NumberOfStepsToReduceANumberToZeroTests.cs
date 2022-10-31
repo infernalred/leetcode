7b8c89357fe9c07e3bfef8d088a0ba12bc900cfb
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-10-31 17:33:09 +0300

    NumberOfStepsToReduceANumberToZero

---
+++ Leetcode/LeetCode.Tests/NumberOfStepsToReduceANumberToZeroTests.cs
@@ -0,0 +1,19 @@
+﻿using Leetcode;
+using Xunit;
+
+namespace LeetCode.Tests;
+
+public class NumberOfStepsToReduceANumberToZeroTests
+{
+    [Fact]
+    public void Test1()
+    {
+        const int input = 14;
+
+        const int expect = 6;
+
+        var actual = NumberOfStepsToReduceANumberToZero.NumberOfSteps(input);
+        
+        Assert.Equal(expect, actual);
+    }
+}
---
+++ Leetcode/Leetcode/NumberOfStepsToReduceANumberToZero.cs
@@ -0,0 +1,23 @@
+﻿namespace Leetcode;
+
+public static class NumberOfStepsToReduceANumberToZero
+{
+    public static int NumberOfSteps(int num)
+    {
+        var result = 0;
+        while (num > 0)
+        {
+            if (num % 2 == 0)
+            {
+                num /= 2;
+            }
+            else
+            {
+                --num;
+            }
+
+            ++result;
+        }
+        return result;
+    }
+}
