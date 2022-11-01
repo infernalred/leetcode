0dddd2fe3840b41fbdd56d1648336ba87a3c671f
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-11-01 17:36:27 +0300

    Longest Substring Without Repeating Characters

---
+++ Leetcode/LeetCode.Tests/LengthOfLongestSubstringTests.cs
@@ -0,0 +1,17 @@
+﻿using Leetcode;
+using Xunit;
+
+namespace LeetCode.Tests;
+
+public class LengthOfLongestSubstringTests
+{
+    [Fact]
+    public void Test1()
+    {
+        const string s = "abcabcbb";
+        const int target = 3;
+        var actual = LongestSubstringWithoutRepeatingCharacters.LengthOfLongestSubstring(s);
+        
+        Assert.Equal(target, actual);
+    }
+}
---
+++ Leetcode/Leetcode/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,28 @@
+﻿namespace Leetcode;
+
+public static class LongestSubstringWithoutRepeatingCharacters
+{
+    public static int LengthOfLongestSubstring(string s)
+    {
+        var left = 0;
+        var right = 0;
+        var result = 0;
+        var dic = new Dictionary<char, bool>();
+        while (right < s.Length)
+        {
+            if (!dic.ContainsKey(s[right]) || dic[s[right]] == false)
+            {
+                result = Math.Max(result, right - left + 1);
+                dic[s[right]] = true;
+                right++;
+            }
+            else
+            {
+                dic[s[left]] = false;
+                left++;
+            }
+        }
+
+        return result;
+    }
+}
