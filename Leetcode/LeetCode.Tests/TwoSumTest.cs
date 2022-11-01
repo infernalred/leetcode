32f82766d49b27144e0a8a357da37689b122030f
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-11-01 14:28:57 +0300

    Two Sum

---
+++ Leetcode/LeetCode.Tests/TwoSumTest.cs
@@ -0,0 +1,37 @@
+﻿using Leetcode;
+using Xunit;
+
+namespace LeetCode.Tests;
+
+public class TwoSumTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var nums = new int[]{2, 7, 11, 15};
+        var target = 9;
+        var actual = TwoSumTask.TwoSum(nums, target);
+        
+        Assert.Equal(new int[]{0,1}, actual);
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        var nums = new int[]{3,2,4};
+        var target = 6;
+        var actual = TwoSumTask.TwoSum(nums, target);
+        
+        Assert.Equal(new int[]{1,2}, actual);
+    }
+    
+    [Fact]
+    public void Test3()
+    {
+        var nums = new int[]{3,2,3};
+        var target = 6;
+        var actual = TwoSumTask.TwoSum(nums, target);
+        
+        Assert.Equal(new int[]{0,2}, actual);
+    }
+}
---
+++ Leetcode/Leetcode/TwoSumTask.cs
@@ -0,0 +1,21 @@
+﻿namespace Leetcode;
+
+public static class TwoSumTask
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        var visited = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (visited.ContainsKey(target - nums[i]))
+            {
+                return new[] {visited[target - nums[i]], i};
+            }
+
+            visited[nums[i]] = i;
+        }
+
+        return new int[2];
+    }
+}
