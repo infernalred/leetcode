ea5918d091339098a8e1b7067a2de2a4a666fe66
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-11-03 11:35:37 +0300

    LongestPalindrome

---
+++ Leetcode/LeetCode.Tests/LongestPalindromicSubstringTests.cs
@@ -0,0 +1,15 @@
+﻿using Leetcode;
+using Xunit;
+
+namespace LeetCode.Tests;
+
+public class LongestPalindromicSubstringTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var actual = LongestPalindromicSubstring.LongestPalindrome("babad");
+        
+        Assert.Equal("bab", actual);
+    }
+}
---
+++ Leetcode/Leetcode/LongestPalindromicSubstring.cs
@@ -0,0 +1,34 @@
+﻿namespace Leetcode;
+
+public static class LongestPalindromicSubstring
+{
+    public static string LongestPalindrome(string s)
+    {
+        var start = 0;
+        var length = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            Find(s, i, i, ref start, ref length);
+            
+            Find(s, i, i + 1, ref start, ref length);
+        }
+
+        return s.Substring(start, length);
+    }
+
+    private static void Find(string s, int left, int right, ref int start, ref int length)
+    {
+        while (left >= 0 && right < s.Length && s[left] == s[right])
+        {
+            if (right - left + 1 > length)
+            {
+                start = left;
+                length = right - left + 1;
+            }
+
+            left--;
+            right++;
+        }
+    }
+}
