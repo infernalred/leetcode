8bc8dbd5bb13afe4488d2e3ad0e2cc6e0d8cfea7
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-11-04 21:28:51 +0300

    Longest Common Prefix

---
+++ Leetcode/LeetCode.Tests/LongestCommonPrefixTests.cs
@@ -0,0 +1,47 @@
+﻿using Leetcode;
+using Xunit;
+
+namespace LeetCode.Tests;
+
+public class LongestCommonPrefixTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var actual = LongestCommonPrefix.GetLongestCommonPrefix(new []{"flower","flow","flight"});
+        
+        Assert.Equal("fl", actual);
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        var actual = LongestCommonPrefix.GetLongestCommonPrefix(new []{"dog","racecar","car"});
+        
+        Assert.Equal("", actual);
+    }
+    
+    [Fact]
+    public void Test3()
+    {
+        var actual = LongestCommonPrefix.GetLongestCommonPrefix(new []{"a"});
+        
+        Assert.Equal("a", actual);
+    }
+    
+    [Fact]
+    public void Test4()
+    {
+        var actual = LongestCommonPrefix.GetLongestCommonPrefix(new []{"ab", "a"});
+        
+        Assert.Equal("a", actual);
+    }
+    
+    [Fact]
+    public void Test5()
+    {
+        var actual = LongestCommonPrefix.GetLongestCommonPrefix(new []{"flower","flower","flower","flower"});
+        
+        Assert.Equal("flower", actual);
+    }
+}
---
+++ Leetcode/Leetcode/LongestCommonPrefix.cs
@@ -0,0 +1,25 @@
+﻿namespace Leetcode;
+
+public static class LongestCommonPrefix
+{
+    public static string GetLongestCommonPrefix(string[] strs)
+    {
+        var minWord = strs.FirstOrDefault();
+        if (string.IsNullOrEmpty(minWord)) return string.Empty;
+        var index = 0;
+        for (int i = 0; i < minWord.Length; i++)
+        {
+            for (int j = 1; j < strs.Length; j++)
+            {
+                if (i >= strs[j].Length || strs[j][i] != minWord[i])
+                {
+                    return minWord.Substring(0, index);
+                }
+            }
+
+            index++;
+        }
+
+        return minWord.Substring(0, index);
+    }
+}
