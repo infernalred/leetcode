8378ef07a02fcd021590877a1729e917b3ad70d5
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-11-05 15:38:35 +0300

    Valid Parentheses

---
+++ Leetcode/LeetCode.Tests/ValidParenthesesTests.cs
@@ -0,0 +1,55 @@
+﻿using Leetcode;
+using Xunit;
+
+namespace LeetCode.Tests;
+
+public class ValidParenthesesTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var actual = ValidParentheses.IsValid("()");
+        
+        Assert.True(actual);
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        var actual = ValidParentheses.IsValid("()[]{}");
+        
+        Assert.True(actual);
+    }
+    
+    [Fact]
+    public void Test3()
+    {
+        var actual = ValidParentheses.IsValid("(]");
+        
+        Assert.False(actual);
+    }
+    
+    [Fact]
+    public void Test4()
+    {
+        var actual = ValidParentheses.IsValid("()");
+        
+        Assert.True(actual);
+    }
+    
+    [Fact]
+    public void Test5()
+    {
+        var actual = ValidParentheses.IsValid("]");
+        
+        Assert.False(actual);
+    }
+    
+    [Fact]
+    public void Test6()
+    {
+        var actual = ValidParentheses.IsValid("(])");
+        
+        Assert.False(actual);
+    }
+}
---
+++ Leetcode/Leetcode/ValidParentheses.cs
@@ -0,0 +1,41 @@
+﻿namespace Leetcode;
+
+public static class ValidParentheses
+{
+    public static bool IsValid(string s)
+    {
+        var table = new HashSet<char>
+        {
+            '(', '[', '{'
+        };
+        var valid = new Dictionary<char, char>
+        {
+            {'(', ')' },
+            {')', '(' },
+            {'[', ']' },
+            {']', '[' },
+            {'{', '}' },
+            {'}', '{' }
+        };
+        
+        var stack = new Stack<char>();
+
+        foreach (var t in s)
+        {
+            if (table.Contains(t))
+            {
+                stack.Push(t);
+            }
+            else
+            {
+                stack.TryPop(out var letter);
+                if (letter != valid[t])
+                {
+                    return false;
+                }
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
