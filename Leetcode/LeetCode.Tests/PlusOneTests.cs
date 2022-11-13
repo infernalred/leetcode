605652e3e0b302bd3d8ad23f81c088388d9420cc
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-11-13 19:21:07 +0300

    PlusOne

---
+++ Leetcode/LeetCode.Tests/PlusOneTests.cs
@@ -0,0 +1,31 @@
+﻿using Leetcode;
+using Xunit;
+
+namespace LeetCode.Tests;
+
+public class PlusOneTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var actual = PlusOne.GetPlusOne(new []{1,2,3});
+        
+        Assert.Equal(new []{1,2,4}, actual);
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        var actual = PlusOne.GetPlusOne(new []{4,3,2,1});
+        
+        Assert.Equal(new []{4,3,2,2}, actual);
+    }
+    
+    [Fact]
+    public void Test3()
+    {
+        var actual = PlusOne.GetPlusOne(new []{9});
+        
+        Assert.Equal(new []{1,0}, actual);
+    }
+}
