e137f4c6529e0cabd1cbafba431e7317dcbf4858
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-11-13 20:24:29 +0300

    Create LinkedListCycle.cs

---
+++ Leetcode/Leetcode/LinkedListCycle.cs
@@ -0,0 +1,23 @@
+﻿namespace Leetcode;
+
+public class LinkedListCycle
+{
+    public bool HasCycle(ListNode head)
+    {
+        var fast = head?.next;
+        var slow = head;
+
+        while (fast?.next != null)
+        {
+            if (fast == slow)
+            {
+                return true;
+            }
+
+            slow = slow?.next;
+            fast = fast.next.next;
+        }
+
+        return false;
+    }
+}
