d5345148632bbc5b3b7507c29a8162f2966ff8a8
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-11-19 18:26:29 +0300

    Create ValidPalindrome.cs

---
+++ Leetcode/Leetcode/ValidPalindrome.cs
@@ -0,0 +1,35 @@
+﻿namespace Leetcode;
+
+public class ValidPalindrome
+{
+    public bool IsPalindrome(string s)
+    {
+        var start = 0;
+        var end = s.Length - 1;
+
+        while (start < end)
+        {
+            if (!char.IsLetterOrDigit(s[start]))
+            {
+                start++;
+            }
+            else if (!char.IsLetterOrDigit(s[end]))
+            {
+                end--;
+            }
+            else
+            {
+                if (char.ToLower(s[start]) != char.ToLower(s[end]))
+                {
+                    return false;
+                }
+
+                start++;
+                end--;
+            }
+            
+        }
+
+        return true;
+    }
+}
