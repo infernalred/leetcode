559925bb372814f0b918a016e1cab39e27464b56
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-11-24 17:32:59 +0300

    Symmetric Tree

---
+++ Leetcode/LeetCode.Tests/SymmetricTreeTests.cs
@@ -0,0 +1,28 @@
+﻿using Leetcode;
+using Leetcode.Models;
+using Xunit;
+
+namespace LeetCode.Tests;
+
+public class SymmetricTreeTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var left21 = new TreeNode(3);
+        var right21 = new TreeNode(4);
+        
+        var left22 = new TreeNode(4);
+        var right22 = new TreeNode(3);
+        
+        var left1 = new TreeNode(2, left21, right21);
+        var right1 = new TreeNode(2, left22, right22);
+
+        var root = new TreeNode(1, left1, right1);
+
+        var service = new SymmetricTree();
+        var actual = service.IsSymmetric(root);
+        
+        Assert.True(actual);
+    }
+}
---
+++ Leetcode/Leetcode/SymmetricTree.cs
@@ -0,0 +1,24 @@
+﻿using Leetcode.Models;
+
+namespace Leetcode;
+
+public class SymmetricTree
+{
+    public bool IsSymmetric(TreeNode root)
+    {
+        if (root == null) return false;
+
+        return IsSymmetricSubTree(root.left, root.right);
+    }
+
+    bool IsSymmetricSubTree(TreeNode left, TreeNode right)
+    {
+        if (left != null && right != null)
+        {
+            return left.val == right.val 
+                   && IsSymmetricSubTree(left.left, right.right) 
+                   && IsSymmetricSubTree(left.right, right.left);
+        }
+        return left == right;
+    }
+}
