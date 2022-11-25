ca923d25fc57e24b84a198c62591a996036ee9e6
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-11-25 12:08:00 +0300

    Range Sum of BST

---
+++ Leetcode/LeetCode.Tests/RangeSumOfBSTTests.cs
@@ -0,0 +1,50 @@
+﻿using Leetcode;
+using Leetcode.Models;
+using Xunit;
+
+namespace LeetCode.Tests;
+
+public class RangeSumOfBSTTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var left01 = new TreeNode(1);
+        var left11 = new TreeNode(6);
+        
+        var left21 = new TreeNode(3, left: left01);
+        var right21 = new TreeNode(7, right: left11);
+
+        var left22 = new TreeNode(13);
+        var right22 = new TreeNode(18);
+        
+        var left1 = new TreeNode(5, left21, right21);
+        var right1 = new TreeNode(15, left22, right22);
+
+        var root = new TreeNode(10, left1, right1);
+
+        var service = new RangeSumOfBST();
+        var actual = service.RangeSumBST(root, 6, 10);
+        
+        Assert.Equal(23, actual);
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        var left21 = new TreeNode(3);
+        var right21 = new TreeNode(7);
+
+        var right22 = new TreeNode(18);
+        
+        var left1 = new TreeNode(5, left21, right21);
+        var right1 = new TreeNode(15, null, right22);
+
+        var root = new TreeNode(10, left1, right1);
+
+        var service = new RangeSumOfBST();
+        var actual = service.RangeSumBST(root, 7, 15);
+        
+        Assert.Equal(32, actual);
+    }
+}
---
+++ Leetcode/Leetcode/RangeSumOfBST.cs
@@ -0,0 +1,34 @@
+﻿using Leetcode.Models;
+
+namespace Leetcode;
+
+public class RangeSumOfBST
+{
+    public int RangeSumBST(TreeNode root, int low, int high)
+    {
+        return RangeSumBstTree(root, low, high);
+    }
+
+    int RangeSumBstTree(TreeNode root, int low, int high)
+    {
+        if (root == null) return 0;
+
+        var sum = 0;
+        if (root.val > low)
+        {
+            sum += RangeSumBstTree(root.left, low, high);
+        } 
+
+        if (root.val < high)
+        {
+            sum += RangeSumBstTree(root.right, low, high);
+        }
+        // var sum = RangeSumBstTree(root.left, low, high) + RangeSumBstTree(root.right, low, high);
+        if (root.val >= low && root.val <= high)
+        {
+            return sum + root.val;
+        }
+
+        return sum;
+    }
+}
