5d845f8773e8cb4ba045b181793a7855d24cf929
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-12-01 14:22:29 +0300

    Create MaximumDepthOfBinaryTree.cs

---
+++ Leetcode/Leetcode/MaximumDepthOfBinaryTree.cs
@@ -0,0 +1,22 @@
+﻿using Leetcode.Models;
+
+namespace Leetcode;
+
+public class MaximumDepthOfBinaryTree
+{
+    public int MaxDepth(TreeNode root)
+    {
+        var depth = 0;
+        
+        if (root == null) return depth;
+
+        depth++;
+
+        if (root.left == null && root.right == null)
+            return depth;
+
+        depth += Math.Max(MaxDepth(root.left), MaxDepth(root.right));
+
+        return depth;
+    }
+}
