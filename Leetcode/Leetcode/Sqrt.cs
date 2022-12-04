cbe9b4c3f3817182bc7b07e2cf3f1566ae5b0b59
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-12-04 19:30:21 +0300

    Create Sqrt.cs

---
+++ Leetcode/Leetcode/Sqrt.cs
@@ -0,0 +1,16 @@
+﻿namespace Leetcode;
+
+public class Sqrt
+{
+    public int MySqrt(int x)
+    {
+        long result = x;
+
+        while (result * result > x)
+        {
+            result = (result + x / result) / 2;
+        }
+
+        return (int)result;
+    }
+}
