cbaa953b4e5866753bc14a59472dd4f25b50e94c
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-12-08 16:37:26 +0300

    MajorityElement

---
+++ Leetcode/LeetCode.Tests/MajorityElementTest.cs
@@ -0,0 +1,23 @@
+﻿using Leetcode;
+using Xunit;
+
+namespace LeetCode.Tests;
+
+public class MajorityElementTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var actual = MajorityElement.GetMajorityElement(new []{3,2,3});
+        
+        Assert.Equal(3, actual);
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        var actual = MajorityElement.GetMajorityElement(new []{2,2,1,1,1,2,2});
+        
+        Assert.Equal(2, actual);
+    }
+}
---
+++ Leetcode/Leetcode/MajorityElement.cs
@@ -0,0 +1,20 @@
+﻿namespace Leetcode;
+
+public static class MajorityElement
+{
+    public static int GetMajorityElement(int[] nums)
+    {
+        var r = 0;
+        var c = 0;
+
+        foreach (var num in nums)
+        {
+            if (c == 0)
+                r = num;
+
+            c += num == r ? 1 : -1;
+        }
+
+        return r;
+    }
+}
