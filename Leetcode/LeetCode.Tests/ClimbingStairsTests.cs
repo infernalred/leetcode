5791ac209e67dc5549bebd0d2f7c1eb3b9cc8cce
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-12-10 16:28:18 +0300

    ClimbingStairs

---
+++ Leetcode/LeetCode.Tests/ClimbingStairsTests.cs
@@ -0,0 +1,23 @@
+﻿using Leetcode;
+using Xunit;
+
+namespace LeetCode.Tests;
+
+public class ClimbingStairsTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var actual = ClimbingStairs.ClimbStairs(2);
+        
+        Assert.Equal(2, actual);
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        var actual = ClimbingStairs.ClimbStairs(3);
+        
+        Assert.Equal(3, actual);
+    }
+}
---
+++ Leetcode/Leetcode/ClimbingStairs.cs
@@ -0,0 +1,19 @@
+﻿namespace Leetcode;
+
+public static class ClimbingStairs
+{
+    public static int ClimbStairs(int n)
+    {
+        var one = 1;
+        var two = 1;
+
+        for (var i = 0; i < n - 1; i++)
+        {
+            var temp = one;
+            one += two;
+            two = temp;
+        }
+
+        return one;
+    }
+}
