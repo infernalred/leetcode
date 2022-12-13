92dadc962f90b429a75c4f6fc6cdd648ce840f13
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-12-13 14:13:38 +0300

    PascalsTriangle

---
+++ Leetcode/LeetCode.Tests/PascalsTriangleTests.cs
@@ -0,0 +1,55 @@
+﻿using Leetcode;
+using Xunit;
+
+namespace LeetCode.Tests;
+
+public class PascalsTriangleTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var actual = PascalSTriangle.Generate(5);
+
+        var expected = new List<List<int>>
+        {
+            new()
+            {
+                1
+            },
+            new()
+            {
+                1,1
+            },
+            new()
+            {
+                1,2,1
+            },
+            new()
+            {
+                1,3,3,1
+            },
+            new()
+            {
+                1,4,6,4,1
+            },
+        };
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var actual = PascalSTriangle.Generate(1);
+
+        var expected = new List<List<int>>
+        {
+            new()
+            {
+                1
+            }
+        };
+
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Leetcode/Leetcode/PascalSTriangle.cs
@@ -0,0 +1,21 @@
+﻿namespace Leetcode;
+
+public static class PascalSTriangle
+{
+    public static IList<IList<int>> Generate(int numRows)
+    {
+        var res = new List<IList<int>> {new List<int>{1}};
+
+        for (int i = 1; i < numRows; i++)
+        {
+            var tmp = Enumerable.Repeat(1, res.Count + 1).ToArray();
+            for (int j = 1; j < tmp.Length - 1; j++)
+            {
+                tmp[j] = res[^1][j - 1] + res[^1][j];
+            }
+            res.Add(tmp);
+        }
+
+        return res;
+    }
+}
