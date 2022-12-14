b4c822d2643756a86005b4510bac6d1be9b5cb27
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-12-14 13:33:08 +0300

    Create FirstUniqueCharacterInAString.cs

---
+++ Leetcode/Leetcode/FirstUniqueCharacterInAString.cs
@@ -0,0 +1,16 @@
+﻿namespace Leetcode;
+
+public class FirstUniqueCharacterInAString
+{
+    public int FirstUniqChar(string s)
+    {
+        foreach (var letter in s.Distinct())
+        {
+            if (s.Count(x => x == letter) == 1)
+            {
+                return s.IndexOf(letter);
+            }
+        }
+        return -1;
+    }
+}
