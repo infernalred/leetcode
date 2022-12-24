b01e1fd2a9c62cd7a8e000294f5434e5dc18b1e6
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-12-24 19:35:56 +0300

    MissingNumbe

---
+++ Leetcode/LeetCode.Tests/MissingNumberTests.cs
@@ -0,0 +1,23 @@
+﻿using Leetcode;
+using Xunit;
+
+namespace LeetCode.Tests;
+
+public class MissingNumberTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var actual = MissingNumber.GetMissingNumber(new []{3,0,1});
+        
+        Assert.Equal(2, actual);
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        var actual = MissingNumber.GetMissingNumber(new []{9,6,4,2,3,5,7,0,1});
+        
+        Assert.Equal(8, actual);
+    }
+}
---
+++ Leetcode/Leetcode/MissingNumber.cs
@@ -0,0 +1,9 @@
+﻿namespace Leetcode;
+
+public static class MissingNumber
+{
+    public static int GetMissingNumber(int[] nums)
+    {
+        return nums.Length + nums.Select((t, i) => i - t).Sum();
+    }
+}
