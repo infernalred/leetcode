8ca7b961ed7c7932b16420b72766cdec3651451d
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-12-25 18:11:36 +0300

    Create ExcelSheetColumnNumber.cs

---
+++ Leetcode/Leetcode/ExcelSheetColumnNumber.cs
@@ -0,0 +1,14 @@
+﻿namespace Leetcode;
+
+public class ExcelSheetColumnNumber
+{
+    public int TitleToNumber(string columnTitle)
+    {
+        var result = 0;
+        foreach (var c in columnTitle)
+        {
+            result = result * 26 + c - 'A' + 1;
+        }
+        return result;
+    }
+}
