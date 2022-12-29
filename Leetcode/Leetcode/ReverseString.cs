e31487ea768ca1a9d53812fe905da56774fc6879
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2022-12-29 19:01:51 +0300

    Create ReverseString.cs

---
+++ Leetcode/Leetcode/ReverseString.cs
@@ -0,0 +1,16 @@
+﻿namespace Leetcode;
+
+public class ReverseString
+{
+    public void ReverseStringMethod(char[] s)
+    {
+        var left = 0;
+        var right = s.Length - 1;
+        while (left < right)
+        {
+            (s[left], s[right]) = (s[right], s[left]);
+            left++;
+            right--;
+        }
+    }
+}
