9589e6310eefeec6e65371dbf6e18fbaaec8558b
Aleksandr Larochkin <39046593+infernalred@users.noreply.github.com>
2023-01-01 12:38:59 +0300

    ValidAnagram

---
+++ Leetcode/LeetCode.Tests/ValidAnagramTests.cs
@@ -0,0 +1,15 @@
+﻿using Leetcode;
+using Xunit;
+
+namespace LeetCode.Tests;
+
+public class ValidAnagramTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var actual = ValidAnagram.IsAnagram("anagram", "nagaram");
+        
+        Assert.True(actual);
+    }
+}
---
+++ Leetcode/Leetcode/ValidAnagram.cs
@@ -0,0 +1,9 @@
+﻿namespace Leetcode;
+
+public class ValidAnagram
+{
+    public static bool IsAnagram(string s, string t)
+    {
+        return s.OrderBy(x => x).SequenceEqual(t.OrderBy(x => x));
+    }
+}
